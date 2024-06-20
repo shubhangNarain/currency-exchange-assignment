008edf843e62943549d23ffea741d6c1eac8acbe
shubhangNarain <snvishere@gmail.com>
2024-06-20 13:45:54 +0530

    almost done, will finish it soon

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,41 @@ function App() {
 
   const [from, setFrom] = useState("EUR");
   const [to, setTo] = useState("USD");
-  const [amount, setAmount] = useState(0)
+  const [amount, setAmount] = useState(0);
+  // const [exchangeRate, setExchangeRate] = useState(0);
+  var exchangeRate = 0;
+  // const exchangeRate = (Math.random * 6) - 3;
 
-  const exchangeRate = (Math.random * 6) - 3;
+  const fluctation = () => {
+    exchangeRate = exchangeRate + Math.random * 6 - 3;
+  };
 
   useEffect(() => {
     setTimeout(() => {
       setEurUsd(eurUsd + eurUsd * exchangeRate.toFixed(2));
-      // setUsdInr(usdInr + usdInr * exchangeRate.to);
-      // setAudUsd(audUsd + audUsd * exchangeRate);
+      setUsdInr(usdInr + usdInr * 0.1);
+      setAudUsd(audUsd + audUsd * 0.1);
+      // fluctation();
     }, 1000);
-  }, [eurUsd]);
+    console.log({ usdInr, eurUsd, audUsd, exchangeRate });
+  }, [eurUsd, exchangeRate, usdInr, audUsd]);
 
-  var convertedAmount
+  var convertedAmount;
 
   const handleExchange = () => {
-    convertedAmount = amount * to / from;
-    console.log("Converted:",convertedAmount);
-  }
+    if (from === "USD" && to === "INR") {
+      convertedAmount = amount * usdInr;
+    }
+    if (from === "EUR" && to === "USD") {
+      convertedAmount = amount * eurUsd;
+    }
+    if (from === "AUD" && to === "USD") {
+      convertedAmount = amount * audUsd;
+    }
+    if (amount === 0) convertedAmount = 0;
+    else alert("Invalid conversion Currencies");
+    console.log("Converted:", convertedAmount);
+  };
 
   return (
     <>
@@ -74,11 +91,13 @@ function App() {
             </div>
           </div>
         </div>
+
         <div className="flex items-center justify-center">
           <div className="rounded-xl h-[500px] w-[500px] flex flex-col bg-gray-300">
-            <div className="p-12">Currency converter</div>
+            <div className="p-12 font-bold text-2xl">Currency converter</div>
             <div className="px-8">
-              <div className="p-2">
+              <div className="p-2 flex flex-col">
+                From Currency
                 <select value={from} onChange={(e) => setFrom(e.target.value)}>
                   {currencies.map((currency) => (
                     <option key={currency} value={currency}>
@@ -88,24 +107,36 @@ function App() {
                   {console.log("From:", from)}
                 </select>
               </div>
-              <div className="p-2">
+              <div className="p-2 flex flex-col">
+                To Currency
                 <select value={to} onChange={(e) => setTo(e.target.value)}>
                   {currencies.map((currency) => (
                     <option key={currency} value={currency}>
                       {currency}
                     </option>
                   ))}
-                  {console.log("To", to)}
+                  {console.log("To:", to)}
                 </select>
               </div>
-              
-              <div>Amount:</div>
-              <input value={amount} onChange={e => setAmount(e.target.value)} className="w-full h-8 p-2" placeholder="Amount"/>
-              {console.log(amount)}
 
+              <div className="p-2">Amount:</div>
+              <div className="p-2">
+                <input
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                  className="w-full h-8 p-2"
+                  placeholder="Amount"
+                />
+                {console.log(amount)}
+              </div>
 
-              <div className="p-5 rounded-md bg-gray-600 h-16">Estimated converted amount: {convertedAmount || 0}</div>
+              <div className="p-2">
+              <div className="p-5 rounded-md bg-gray-600 h-16">
+                Estimated converted amount: {convertedAmount}
+              </div>
+              </div>
 
+              <div className="p-2">
               <button
                 type="button"
                 className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
@@ -113,6 +144,7 @@ function App() {
               >
                 Exchange
               </button>
+              </div>
             </div>
           </div>
         </div>
